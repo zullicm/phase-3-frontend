236e6c18f569b91560a9c5b7ebaeb3c3ca4de8b3
Christopher Marzulli <chriszulli15@gmail.com>
2022-09-20 11:23:30 -0400

    added state to both inputs

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -2,9 +2,10 @@ import React, {useState, useEffect} from "react";
 import Review from "./Review";
 
 function MovieCard({ movie }){
-  const {name, image} = movie
-
+  const {name, image, id} = movie
   const [reviews, setReviews] = useState([])
+  const [userName, setUserName] = useState("")
+  const [comment, setComment] = useState("")
   
   function reviewSetter(data){
     setReviews([...reviews, data])
@@ -16,10 +17,23 @@ function MovieCard({ movie }){
     .then(data => reviewSetter(data))
   }, [])
 
+  function handleSubmit(e){
+
+  }
+
+  function handleName(e){
+    setUserName(e.target.value)
+  }
+
+  function handleComment(e){
+    setComment(e.target.value)
+  }
+
+
   return(
     <div className="movie-card">
       <div className="reviews">
-        {reviews.map(review => <Review/>)}
+        {reviews.map(review => <Review />)}
       </div>
       <div className="ticket-left">
       <div className="movie-info">
@@ -27,9 +41,10 @@ function MovieCard({ movie }){
       </div>
       <div className="movie-input">
         <form>
-          <input type="text" name="name" placeholder=" Your Name"></input>
-          <input type="text" name="comment" placeholder=" Your Witty Comment"></input>
-          <a class="waves-effect waves-light btn">Leave Comment</a>
+          <input type="text" name="name" placeholder=" Your Name" onChange={handleName}
+           value={userName}></input>
+          <input type="text" name="comment" placeholder=" Your Witty Comment" onChange={handleComment} value={comment}></input>
+          <a onClick={handleSubmit} class="waves-effect waves-light btn">Leave Comment</a>
         </form>
       </div>
       </div>
