4e6266bab6ed0b6ecee7772e46d50eaea0d54214
Christopher Marzulli <chriszulli15@gmail.com>
2022-09-21 10:53:07 -0400

    Added a form to the movie add oage and added state

--- src/Components/AddMoviePage.js
+++ src/Components/AddMoviePage.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddMoviePage(){
+  const [name, setName] = useState("")
+  const [image, setImage] = useState("")
+
+
+  function handleName(e){
+    setName(e.target.value)
+  }
+  function handleImage(e){
+    setImage(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+  }
 
   return(
     <>
       <h1>
         AddMoviePage
       </h1>
+      <div className="movie-form">
+        <form>
+          <input type="text" name="name" placeholder=" Your Name" onChange={handleName}
+           value={name}></input>
+          <input type="text" name="comment" placeholder=" Your Witty Comment" onChange={handleImage} value={image}></input>
+          <a onClick={handleSubmit} className="waves-effect waves-light btn">Leave Comment</a>
+        </form>
+      </div>
     </>
   )
 }
